d97bb71a6c4ef7dd481a61091a8fd232a5a1a735
Isaac Zinman <isaaczinman@Matthews-MacBook-Pro.local>
2018-11-09 18:50:45 -0800

    Added placeholder AR view IZ

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,18 +18,37 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  switchAR(){
+    this.setState({ARview: 1});
+  }
+  constructor(props) {
+    super(props);
+    this.state = {ARview: 0};
+    this.switchAR = this.switchAR.bind(this);
+  }
   render() {
+    if (this.state.ARview == 1) return (
+      <View style={styles.container}>
+        <Text style={styles.meme}>I'm an ar view lol</Text>
+      </View>
+    )
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.meme}>s p a c e b o i s</Text>
+        <Button style={styles.instructions} title='Click here for MAGICAL AUGMENTED REALITY' onPress={this.switchAR}></Button>
       </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
+  meme: {
+    fontSize: 50,
+    justifyContent: 'center',
+    color: '#000000',
+    margin: 10,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
