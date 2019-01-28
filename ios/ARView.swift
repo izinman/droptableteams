dea1cde5649ae6c052839fdc403141486ce8ad93
Cole Margerum <Cole@Cole-Ms-MacBook-Pro.local>
2019-01-28 14:59:18 -0800

    Adjusting D Pad Based on Camera Yaw

--- ios/ARView.swift
+++ ios/ARView.swift
@@ -16,6 +16,9 @@ class ARView : ARSCNView, ARSCNViewDelegate {
     
     var selectedNode: SCNNode?
     var onObjectSelect: RCTDirectEventBlock?
+    var cameraVector: simd_float3 {
+        return session.currentFrame!.camera.eulerAngles
+    }
     
     let ObjScaleMap: [String: SCNVector3] = [
         "chair": SCNVector3(x: 0.02, y: 0.02, z: 0.02),
@@ -62,7 +65,7 @@ class ARView : ARSCNView, ARSCNViewDelegate {
         let nodeHits = self.hitTest(location, options: nil)
         
         // Check that the node is not null and select it
-        if let node = nodeHits.first?.node as? SCNNode, objects.contains(node) {
+        if let node = nodeHits.first?.node, objects.contains(node) {
             // If we had previously selected an object, unhighlight it before selecting a new one
             if (selectedNode != nil) {
                 selectedNode?.opacity = 1.0
@@ -81,6 +84,16 @@ class ARView : ARSCNView, ARSCNViewDelegate {
         onObjectSelect = val
     }
     
+    func getAdjustedCordinates(x: Float, z: Float) -> (CGFloat, CGFloat) {
+        // z2 = cosβz_1 − sinβx_1
+        // x2 = sinβz_1 + cosβx_1
+        let beta: Float = cameraVector.y;
+        let adjustedZ = CGFloat(cos(beta) * z - sin(beta) * x)
+        let adjustedX = CGFloat(sin(beta) * z + cos(beta) * x)
+        
+        return (adjustedX, adjustedZ)
+    }
+    
     func adjustObject(buttonPressed: String) {
         
         switch (buttonPressed) {
@@ -92,27 +105,31 @@ class ARView : ARSCNView, ARSCNViewDelegate {
             selectedNode?.runAction(SCNAction.rotateBy(x: 0, y: 0.1, z: 0, duration: 0))
             
         case "moveLeft":
-            selectedNode?.runAction(SCNAction.moveBy(x: -0.01, y: 0, z: 0, duration: 0))
+            let adjustedCorrdinates = getAdjustedCordinates(x: -0.01, z: 0)
+            selectedNode?.runAction(SCNAction.moveBy(x: adjustedCorrdinates.0, y: 0, z: adjustedCorrdinates.1, duration: 0))
             
         case "moveRight":
-            selectedNode?.runAction(SCNAction.moveBy(x: 0.01, y: 0, z: 0, duration: 0))
+            let adjustedCorrdinates = getAdjustedCordinates(x: 0.01, z: 0)
+            selectedNode?.runAction(SCNAction.moveBy(x: adjustedCorrdinates.0, y: 0, z: adjustedCorrdinates.1, duration: 0))
             
         case "moveForward":
-            selectedNode?.runAction(SCNAction.moveBy(x: 0, y: 0, z: 0.01, duration: 0))
+            let adjustedCorrdinates = getAdjustedCordinates(x: 0, z: -0.01)
+            selectedNode?.runAction(SCNAction.moveBy(x: adjustedCorrdinates.0, y: 0, z: adjustedCorrdinates.1, duration: 0))
             
         case "moveBackward":
-            selectedNode?.runAction(SCNAction.moveBy(x: 0, y: 0, z: -0.01, duration: 0))
+            let adjustedCorrdinates = getAdjustedCordinates(x: 0, z: 0.01)
+            selectedNode?.runAction(SCNAction.moveBy(x: adjustedCorrdinates.0, y: 0, z: adjustedCorrdinates.1, duration: 0))
             
         case "confirmPlacement":
             selectedNode?.opacity = 1.0
             selectedNode = nil
             
         case "deleteObject":
-            if selectedNode != nil, let index = objects.index(of: selectedNode!) {
+            if let node = selectedNode, let index = objects.index(of: node) {
                 objects.remove(at: index)
-                selectedNode?.removeFromParentNode()
-                selectedNode = nil
+                node.removeFromParentNode()
             }
+            selectedNode = nil
             
         default:
             return
