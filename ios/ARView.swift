155bc1882599d4b36f73e5c2d7319a088b44ce40
Isaac Zinman <zinisaac@gmail.com>
2019-02-27 22:53:16 -0800

    Remove evil deadly awful fake ObjScaleMap

--- ios/ARView.swift
+++ ios/ARView.swift
@@ -17,19 +17,4 @@ class ARView : ARSCNView, ARSCNViewDelegate {
     var placeButton: UIButton!
     var confirmButton: UIButton!
     var removeButton: UIButton!
-
-    let ObjScaleMap: [String: SCNVector3] = [
-        "chair": SCNVector3(x: 0.015, y: 0.015, z: 0.015),
-        "vase": SCNVector3(x: 0.0015, y: 0.0015, z: 0.0015),
-        "table_1": SCNVector3(x: 0.01, y: 0.01, z: 0.01),
-        "coffee_table": SCNVector3(x: 0.25, y: 0.25, z: 0.25),
-        "couch_1": SCNVector3(x: 0.02, y: 0.02, z: 0.02),
-        "couch_2": SCNVector3(x: 0.02, y: 0.02, z: 0.02),
-        "couch_3": SCNVector3(x: 0.02, y: 0.02, z: 0.02),
-        "chair_1": SCNVector3(x: 0.02, y: 0.02, z: 0.02),
-        "chair_2": SCNVector3(x: 0.02, y: 0.02, z: 0.02),
-        "wardrobe": SCNVector3(x: 0.02, y: 0.02, z: 0.02)
-        
-    ]
-    
 }
